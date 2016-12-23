8163ccc727b5345887b8645563a29a12db663941
Ethan Seither <ethan.seither@gmail.com>
2016-12-23 08:06:26 -0800

    now I am become John, destroyer of champs

--- johnceenabot/voice_fns.py
+++ johnceenabot/voice_fns.py
@@ -7,21 +7,38 @@ import asyncio
 
 voice_logger = logging.getLogger('voice')
 
+
+
+#honestly isn't this all we need?
+
+class MusicBox(object):
+    def __init__(self, avconv=False):
+        self.player = None
+        self.ended = asyncio.Event()
+
+    def stop_player(self):
+        if self.player and self.player.is_playing():
+            voice_logger.info('Something playing, stopping it')
+            self.player.stop()
+            voice_logger.info('Player stopped')
+        self.ended.set()
+
+
+ourbox = MusicBox()
 @bot.command(pass_context=True)
 async def cena(ctx):
     print(ctx)
+    ended = asyncio.Event()
     #discord.opus.load_opus('opus')
     voice_logger.info('trying to join channel')
-
     if ctx.message.author.voice_channel is not None:
         print(bot)
         voicechn = await bot.join_voice_channel(ctx.message.author.voice_channel)
-        player = voicechn.create_ffmpeg_player('johnceenabot/audio/invisible.mp3')
+        ourbox.ended = asyncio.Event()
+        player = voicechn.create_ffmpeg_player('johnceenabot/audio/invisible.mp3', after=ourbox.stop_player)
         player.start()
+        await ourbox.ended.wait()
+        await voicechn.disconnect()
     else:
         voice_logger.warning('user is not in a channel!')
         print('shit')
-
-
-
-
